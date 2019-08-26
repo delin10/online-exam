f299be69d4396ee0af14ea563de3d20f6baec3fa
delin10 <lidelin10@outlook.com>
2019-08-26 21:29:05 +0800

    update 数据sql

--- sql/book.sql
+++ sql/book.sql
@@ -1,5 +1,5 @@
 -- the base table for user information
-create table `user`(
+create table `t_user`(
     `id` int unsigned auto_increment not null comment 'id',
     `nick_name` varchar(20) comment '',
     `role` int unsigned not null default 0 comment '0-normal user 1-manager',
@@ -10,18 +10,18 @@ create table `user`(
 )engine=innodb default character set utf8mb4;
 
 -- role table, use to auth
-create table `role`(
-    `id` int unsigned auto_increment not null comment '',
+create table `t_role`(
+    `id` int unsigned auto_increment not null comment '0-manager 1-normal user other-',
     `name` varchar(20) not null comment '',
     `desc` varchar(50) comment 'description',
     `create_time` long not null comment 'the time when create user',
     `last_update_time` long not null comment 'the time the record is updated lastly',
     `creator` int unsigned not null,
     primary key(`id`)
-)engine=innodb default character set utf8mb4;
+)engine=innodb default character set utf8mb4 auto_increment=2;
 
 -- the base table for user information
-create table `resource`(
+create table `t_resource`(
     `id` int unsigned auto_increment not null comment 'id',
     `name` varchar(20) comment '',
     `uri` varchar(50) comment 'the uri to identify resource',
@@ -34,7 +34,7 @@ create table `resource`(
 )engine=innodb default character set utf8mb4;
 
 -- the mapping between roles and resources
-create table `role_resource`(
+create table `t_role_resource`(
     `role_id` int unsigned not null,
     `resource_id` int unsigned not null,
     `create_time` long not null comment 'the time when create user',
@@ -42,15 +42,45 @@ create table `role_resource`(
     primary key (`role_id`,`resource_id`)
 )engine=innodb default character set utf8mb4;
 
-
-create table book(
+-- book base info
+create table `t_book`(
     `id` int unsigned auto_increment not null comment 'id',
     `name` varchar(50) not null comment '',
     `category` tinyint not null comment '',
     `price` decimal(15,2) not null comment '',
     'special_price' decimal(15,2) default null comment '',
-    `on_sell` tinyint default 0 comment ''
+    `discount` tinyint default 100 comment '',
+    `on_sell` tinyint default 0 comment '',
+    `cover_url` varchar(50) comment 'cover image url(only one)',
+    `desc` varchar(100) comment '',
+    `create_time` long not null comment 'the time when create user',
+    `last_update_time` long not null comment 'the time the record is updated lastly',
+    primary key (id)
 )engine=innodb default character set utf8mb4;
 
+-- order table
+create table`t_order`(
+    `id` bigint(20) not null comment '',
+    `book_id` int unsigned not null comment '',
+    `user_id` int unsigned not null comment '',
+    `order_status` tinyint default 0 comment '0-processing 1-refund 2-finish',
+    `real_pay` decimal(15,2) not null comment 'real fee which the user has paid',
+    `address` varchar(50) not null comment '',
+    `amount` int unsigned not null default 1 comment '',
+    `expect_arrival_time` long comment '',
+    `create_time` long not null comment 'the time when create user',
+    `last_update_time` long not null comment 'the time the record is updated lastly',
+    primary key (id)
+)engine=innodb default character set utf8mb4;
 
+-- book comment
+create table `t_book_comment`(
+    `id` int unsigned auto_increment not null comment 'id',
+    `user_id` int unsigned not null comment '',
+    `content` text comment '',
+    `star` tinyint not null default 5 comment '1~5',
+    `create_time` long not null comment 'the time when create user',
+    `last_update_time` long not null comment 'the time the record is updated lastly',
+    primary key (id)
+)
 
