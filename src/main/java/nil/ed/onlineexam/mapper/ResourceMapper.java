68c54e8e26efcb1fa1b314a38141a4c2794a5b26
delin10 <lidelin10@outlook.com>
2019-08-30 11:19:58 +0800

    delete/Resource -> Permission

--- src/main/java/nil/ed/onlineexam/mapper/ResourceMapper.java
+++
@@ -1,9 +0,0 @@
-package nil.ed.onlineexam.mapper;
-
-import nil.ed.onlineexam.entity.Resource;
-
-public interface ResourceMapper {
-    int insert(Resource record);
-
-    int insertSelective(Resource record);
-}
