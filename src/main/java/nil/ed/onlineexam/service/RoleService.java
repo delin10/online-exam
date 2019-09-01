886ab19340d40182b324b20d1b325065c05be28f
delin10 <lidelin10@outlook.com>
2019-09-01 21:09:15 +0800

    delete/使用接口和Impl类代替

--- src/main/java/nil/ed/onlineexam/service/RoleService.java
+++ src/main/java/nil/ed/onlineexam/service/RoleService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
 
-@Service
+@Service("xx")
 public class RoleService {
     @Resource
     private RoleMapper roleMapper;
