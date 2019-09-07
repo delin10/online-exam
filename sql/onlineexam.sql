eabb9d87e1c3040d9a926454e03c30bc747e5a16
delin10 <lidelin10@outlook.com>
2019-09-07 19:24:07 +0800

    update/添加参与考试记录表 && 优化t_joined_course表结构

--- sql/onlineexam.sql
+++ sql/onlineexam.sql
@@ -115,7 +115,7 @@ create table `t_joined_course`(
     `id` int unsigned auto_increment not null comment '起标志作用',
     `uid` int unsigned not null comment '参与课程的用户id',
     `cid` int unsigned not null comment '课程id',
-    `score` mediumint default -1 comment '分数，初始化为-1',
+    `score` mediumint default -1 comment '课程分数，不一定为试卷分数，初始化为-1',
     `join_test_time` bigint unsigned default null comment '参加考试的时间',
     `create_time` bigint unsigned not null comment '记录创建时间',
     `update_time` bigint unsigned not null comment '记录更新时间',
@@ -123,6 +123,24 @@ create table `t_joined_course`(
     key (uid),
     key (cid)
 )engine=innodb default character set utf8mb4;
+
+alter table `t_joined_course` drop column `join_test_time`;
+alter table `t_joined_course` add unique key t_joined_course_unique_key_uid_cid(`uid`,`cid`);
+
+-- 参加考试的表
+create table `t_joined_test`(
+    `id` int unsigned auto_increment not null comment '起标志作用',
+    `uid` int unsigned not null comment '参与考试的用户id',
+    `tid` int unsigned not null comment '试卷id',
+    `score` mediumint default -1 comment '分数，初始化为-1',
+    `create_time` bigint unsigned not null comment '记录创建时间',
+    `update_time` bigint unsigned not null comment '记录更新时间',
+    primary key(id),
+    key (uid),
+    key (tid)
+)engine=innodb default character set utf8mb4;
+
+alter table `t_joined_test` add unique index t_joined_test_unique_index_uid_tid(`uid`, `tid`);
 
 -- 提交答案的表
 create table `t_submitted_answer`(
@@ -161,6 +179,11 @@ insert into `t_role` values(1,'manager','',unix_timestamp(current_timestamp(3))*
 insert into `t_role` values(2,'normal','',unix_timestamp(current_timestamp(3))*1000,unix_timestamp(current_timestamp(3))*1000,0);
 
 insert into `t_resource` values (NULL, 'test', '/api/test/{id}', 0,unix_timestamp(current_timestamp(3))*1000,unix_timestamp(current_timestamp(3))*1000, 0);
+insert into `t_resource` values (NULL, 'listQuestion', '/exam/question/list', 0,unix_timestamp(current_timestamp(3))*1000,unix_timestamp(current_timestamp(3))*1000, 0);
+insert into `t_resource` values (NULL, 'addTestPaper', '/exam/testPaper/add', 0,unix_timestamp(current_timestamp(3))*1000,unix_timestamp(current_timestamp(3))*1000, 0);
+insert into `t_resource` values (NULL, 'listTests', '/exam/testPaper/list', 0,unix_timestamp(current_timestamp(3))*1000,unix_timestamp(current_timestamp(3))*1000, 0);
+
+insert into `t_joined_course` values (NULL, 2, 10 , -1, 1,unix_timestamp(current_timestamp(3))*1000,unix_timestamp(current_timestamp(3))*1000);
 
 insert into `t_role_resource` values (NULL, 2, 1, unix_timestamp(current_timestamp(3))*1000, 0);
 
