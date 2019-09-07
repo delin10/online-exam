355bd2597a049a8153f150bb28c0ab1fd090751a
delin10 <lidelin10@outlook.com>
2019-09-07 19:43:25 +0800

    new+add/添加t_Join_test的相关类

---
+++ src/main/java/nil/ed/onlineexam/entity/JoinedTest.java
@@ -0,0 +1,18 @@
+package nil.ed.onlineexam.entity;
+
+import lombok.Data;
+
+@Data
+public class JoinedTest {
+    private Long id;
+
+    private Integer uid;
+
+    private Integer tid;
+
+    private Short score;
+
+    private Long createTime;
+
+    private Long updateTime;
+}
