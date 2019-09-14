5b2af291829ad9d5ffc625acbe9b16c68191f4c4
delin10 <lidelin10@outlook.com>
2019-09-14 20:22:17 +0800

    update/取消缓存

--- src/main/java/nil/ed/onlineexam/security/InvocationSecurityMetadataSourceServiceImpl.java
+++ src/main/java/nil/ed/onlineexam/security/InvocationSecurityMetadataSourceServiceImpl.java
@@ -58,13 +58,14 @@ public class InvocationSecurityMetadataSourceServiceImpl implements SecurityMeta
         if (configAttributeList == null) {
             synchronized(LOCK){
                 if (configAttributeList == null) {
-                    List<Permission> permissionList = permissionMapper.listAllResources();
-                    configAttributeList = new HashMap<>(permissionList.size() + 1, 1);
-                    for (Permission permission : permissionList){
-                        configAttributeList.put(permission.getUri(),Collections.singleton(new SecurityConfig(permission.getUri())));
-                    }
+
                 }
             }
+        }
+        List<Permission> permissionList = permissionMapper.listAllResources();
+        configAttributeList = new HashMap<>(permissionList.size() + 1, 1);
+        for (Permission permission : permissionList){
+            configAttributeList.put(permission.getUri(),Collections.singleton(new SecurityConfig(permission.getUri())));
         }
     }
 }
